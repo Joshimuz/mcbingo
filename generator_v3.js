619d06e74313b2de454b00d4e20c7c9a77d3f406
Joshimuz <Joshimuz@users.noreply.github.com>
2019-02-26 18:35:47 +0000

    Squashed commit of the following:
    commit ed3fc809dc7700ad52d26e38ac82aad61f5bfc5b
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Tue Feb 26 18:24:56 2019 +0000
    
        Added Tag system
    
        Added a Tag system to the bingo goals and generation to address the issues with certain types of goals being overbearing on the sheet.
    
        Now each goal can (and should) have at least one tag attached, which is a basic description of the goal. Tags have a name and a maximum amount. If the maximum amount of tags is already present on the bingo sheet no more will be added. EG: There can only be 2 "RareBiome" tagged goals at any time on the sheet.
    
    commit 43e9d402d7075442a37d3de7e350d9ecd702d863
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Sun Jul 8 13:18:34 2018 +0100
    
        Removed/updated bad tooltips and images
    
    commit f7f51ef2135c0ca46b45468f6f1e1e321f7f802d
    Merge: 7a147b8 5c58970
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Sun Jul 8 12:08:54 2018 +0100
    
        Merge remote-tracking branch 'refs/remotes/origin/master' into develop-josh
    
    commit 7a147b88ca68efb8214d66c4cb12411c5f5d1c8a
    Merge: 2f0a376 c7acc11
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Fri Jul 6 09:56:06 2018 +0100
    
        Merge remote-tracking branch 'refs/remotes/origin/master' into develop-josh
    
    commit 2f0a376bf83b109c6a52a811d88158d8c99bb6c3
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Fri Jul 6 09:52:57 2018 +0100
    
        Minor Goal Tweaks
    
        Tweaked and spread out edible items goal
        Added death reactant to OOB death message
    
    commit e6511d1e301fee83ea9867d7d6d19136a44db7c4
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Fri Jun 29 08:23:37 2018 +0100
    
        More renaming of changed items
    
    commit 2fdb76290a29feba6993ec8aa6c5994e8d012a56
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Thu Jun 28 22:41:28 2018 +0100
    
        Renaming changed items
    
        Wood is now Logs. Bark is now Wood.
    
    commit 11f0a3bf1f06710d8d2ee826a65430aa41292554
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Thu Jun 28 20:05:04 2018 +0100
    
        Minor goal tweaks
    
        Reworded one goal, added a new goal, rebalanced another
    
    commit 24f44a96ee220d48a910be3e7a4522b021715c05
    Merge: 0ac81ba 9f08f50
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Thu Jun 28 16:56:28 2018 +0100
    
        Merge branch 'master' into develop-josh
    
    commit 0ac81ba4e7bf9bfcb8efd0970ed54cdce3922adf
    Author: Joshimuz <Joshimuz@users.noreply.github.com>
    Date:   Thu Jun 28 16:53:51 2018 +0100
    
        Goal Balancing, fixes and new goals
    
        Fixed wrongly named goals (Very Damaged Anvil)
        Changed a few goal's difficulty
        Added a few new goals

--- generator_v3.js
+++ generator_v3.js
@@ -1,5 +1,5 @@
 // This is part of a version currently in development and may be changed at any time.
-
+var counter = 0;
 
 var generator_v3 = function(layout, difficulty, bingoList)
 {
@@ -112,11 +112,15 @@ var generator_v3 = function(layout, difficulty, bingoList)
 		
 		do 
 		{
+			//console.log("Starting do while loop: " + counter);
+			counter++;
 			var cont = true;
 			failSafe++;
 			
 			var rng = Math.floor((Math.random() * bingoList[sheetLayout[i]].length - 1) + 1);
 			var goalCandidate = bingoList[sheetLayout[i]][rng];
+			
+			//console.log(bingoList[sheetLayout[i]][rng].name + " tags: " + bingoList[sheetLayout[i]][rng].tags);
 			
 			// Check if the goal has a frequency modifier
 			if (typeof goalCandidate.frequency !== 'undefined')
@@ -124,9 +128,12 @@ var generator_v3 = function(layout, difficulty, bingoList)
 				// If it does, make it less likely to appear based on the value of frequency
 				if (Math.floor((Math.random() * goalCandidate.frequency) + 1) < goalCandidate.frequency)
 				{
+					//console.log("cont = false, frequency check failed");
 					cont = false;
 				}
 			}
+			
+			var tagCount = [0, 0, 0, 0, 0];
 
 			for (var z=0; z <= 24; z++)
 			{				
@@ -136,28 +143,64 @@ var generator_v3 = function(layout, difficulty, bingoList)
 					if (currentSheet[z].name == goalCandidate.name)
 					{
 						// If it is get a new goal
+						//console.log("cont = false, same goal on sheet");
 						cont = false;
 					}
 					// Check if the goal generated has any anti synergy with anything already on the sheet
 					else if (currentSheet[z].antisynergy == goalCandidate.antisynergy && typeof currentSheet[z].antisynergy !== 'undefined')
 					{
 						// If it is get a new goal
+						//console.log("cont = false, antisynergy on sheet");
 						cont = false;
 					}
 					// Check if the goal generated is a catalyst for anything already on the sheet
 					else if (currentSheet[z].reactant == goalCandidate.catalyst && typeof currentSheet[z].reactant !== 'undefined')
 					{
 						// If it is get a new goal
+						//console.log("cont = false, goal is catalyst for one on sheet");
 						cont = false;
 					}
 					// Check if the goal generated is a reactant for anything already on the sheet
 					else if (currentSheet[z].catalyst == goalCandidate.reactant && typeof currentSheet[z].catalyst !== 'undefined')
 					{
 						// If it is get a new goal
+						//console.log("cont = false, goal is reactant for one on sheet");
 						cont = false;
 					}
+					
+					// Compare all the tags of the current goal with the tags of the current one on the sheet
+					if (goalCandidate.tags != null && currentSheet[z].tags != null)
+					{
+						for (var x = 0, lenX = goalCandidate.tags.length; x < lenX; x++)
+						{
+							for (var y = 0, lenY = currentSheet[z].tags.length; y < lenY; y++)
+							{								
+								if (goalCandidate.tags[x].name == currentSheet[z].tags[y].name)
+								{
+									tagCount[x]++;
+								}
+							}
+						}
+						
+					}
+					
 				}
 			}
+			
+			// If the tag count is higher than the max that tag should have, don't continue
+			if (goalCandidate.tags != null)
+			{
+				for (var x = 0, len = goalCandidate.tags.length; x < len; x++)
+				{
+					if (tagCount[x] > goalCandidate.tags[x].max)
+					{
+						//console.log("cont = false, tag count is " + tagCount[x]);
+						cont = false;
+					}
+					
+				}
+			}
+
  			
 			// If the loop is stuck because no more suitable goals
 			if (failSafe >= 500)
