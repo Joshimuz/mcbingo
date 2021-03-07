Joshimuz/mcbingo

commit fa40258fc763b18da2b267f73d6f842e5a18ebf6

js: hide the whole #bingo on "Hide Table" click
Function updateHidden() hides individual table cells ('#bingo td') to
hide the contents of the bingo sheet when "Hide Table" button is
clicked.  It also goes through all tootlip icons in the #bingo table to
show them when the table is shown (HIDDEN is false-y).  This is done to
change from their default CSS 'visibility: hidden'.  The #bingo table
itself, however, stays visible, showing its background (stone-tile.png).
This background is covered by table cells when the sheet is shown.

So overhaul how the sheet hiding is done to simplify updateHidden().  To
allow hiding the whole #bingo table, move most of its styling (border,
width, etc) to a new wrapper div#bingo-box.  The background image,
however, is moved to a new div#hidden-bingo, which is wrapped around
#hidden-table.  This wrapping allows us to simplify the CSS for position
and size of #hidden-table.  See declaration 'padding: 50px' for
#hidden-bingo.  This makes the width of the stone-textured border around
#hidden-table consistently 50px.  Judging by the old values of width and
margin-top, this was probably the intended positioning of #hidden-table.

The wrapper #hidden-bingo is also placed inside the new #bingo-box, so
it's a sibling of #bingo.  Both #bingo and #hidden-bingo are set to fill
out the inside of #bingo-box (see height and width declarations).

Hide the whole #bingo table and show #hidden-bingo when "Hide Table" is
clicked using new CSS class "hidden", which is added or removed from the
container #bingo-box in the function updateHidden().

bingo.css
@@ -87,7 +87,6 @@ html, body {
 	margin-right: auto;
 	background-color: #EBEBEB;
 	border: 4px solid;
-	position: relative;
 }
 
 #tooltip {
@@ -136,25 +135,44 @@ html, body {
 	top: -5px;
 }
 
-#bingo {
+#bingo-box {
 	width: 700px;
 	height: 600px;
-	margin: 0;
+	box-sizing: border-box;
+	border: 4px solid #FFF;
+	border-radius: 5px;
+}
+
+#bingo, #hidden-bingo {
+	height: 100%;
+	width: 100%;
+}
+
+#bingo {
 	border-spacing: 0px;
-	border: solid 4px #FFF;
 	background-color: #C6C6C6;
 	border-collapse: separate;
-	border-radius: 5px;
-	background-repeat: repeat;
-	background-size: 128px 128px;
-	background-image: url("stone-tile.png");
-	position: relative;
 	-webkit-touch-callout: none;
 	-webkit-user-select: none;
 	-khtml-user-select: none;
 	-moz-user-select: none;
 	-ms-user-select: none;
 	user-select: none;
+}
+
+#hidden-bingo {
+	display: none;
+	background-image: url("stone-tile.png");
+	background-repeat: repeat;
+	background-size: 128px 128px;
+	box-sizing: border-box;
+	padding: 50px;
+}
+.hidden #hidden-bingo {
+	display: block;
+}
+.hidden #bingo {
+	display: none;
 }
 
 #bingo td {
@@ -170,7 +188,6 @@ html, body {
 	color: #000;
 	padding: 5px 5px 5px 7px;
 	position: relative;
-	visibility: hidden;
 }
 
 #bingo td:hover {
@@ -226,19 +243,12 @@ html, body {
 
 #hidden-table {
 	font-family: settingsFont;
-	position: absolute;
-	top: 50px;
-	left: 0;
-	right: 0;
 	background-color: white;
-	margin: auto;
-	width: 540px;
 	color: #000;
 	border: 2px solid #000;
 	padding: 20px;
 	text-align: center;
 	font-size: 0.75em;
-	display: none;
 }
 
 #hidden-table p {

bingo.js
@@ -394,39 +394,21 @@ function changeLayout()
 
 function updateHidden()
 {
+	const box = $("#bingo-box");
+	const hiddenCssClassName = "hidden";
 	const button = document.getElementById("ishidden");
 	if (HIDDEN)
 	{
 		// Hide the goals and change the hidden setting's text
+		box.addClass(hiddenCssClassName);
 		button.innerHTML = "Show Table";
-		$("#bingo td").css("visibility", "hidden");
-		$("#hidden-table").css("display","block");
-		$("#bingo td img").css("visibility", "hidden");
 	}
 	else
 	{
 		HIDDEN = false;
 		// Show the goals and change the hidden setting's text
+		box.removeClass(hiddenCssClassName);
 		button.innerHTML = "Hide Table";
-		$("#bingo td").css("visibility", "visible");
-		$("#hidden-table").css("display","none");
-		
-		for (var i=0; i<25; i++)
-		{
-			var slotId = "#slot"+ (i + 1);
-			
-			if ($(slotId).data("tooltipimg") !== '')
-			{
-				//$(slotId).data("tooltipimg", goal.tooltipimg);
-				$(slotId).children().css("visibility", "visible");
-			}
-			if ($(slotId).data("tooltiptext") !== '')
-			{
-				//$(slotId).data("tooltiptext", goal.tooltiptext);
-				$(slotId).children().css("visibility", "visible");
-			}
-		}
-		//$("#bingo td img").css("visibility", "visible");
 	}
 }
 

index.html
@@ -32,53 +32,57 @@
 			<h1>Minecraft Bingo</h1>
 		</section>
 		<section id="bingo-section">
-			<table id="bingo">
-				<tr>
-					<td id="slot1" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot2" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot3" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot4" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot5" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-				</tr>
-				<tr>
-					<td id="slot6" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot7" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot8" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot9" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot10" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-				</tr>
-				<tr>
-					<td id="slot11" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot12" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot13" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot14" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot15" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-				</tr>
-				<tr>
-					<td id="slot16" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot17" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot18" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot19" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot20" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-				</tr>
-				<tr>
-					<td id="slot21" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot22" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot23" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot24" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-					<td id="slot25" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
-				</tr>
-			</table>
-			<div id="hidden-table">
-				<div id="hidden-table-seed">
-					Bingo Seed:<br>
-					<input class="seed_for_copying" id="seed_for_copying_hidden" type="text" value="12345" readonly="readonly"><br>
-					<button onclick='copySeedToClipboard("seed_for_copying_hidden");'>Copy Seed to Clipboard</button>
+			<div id="bingo-box">
+				<table id="bingo">
+					<tr>
+						<td id="slot1" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot2" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot3" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot4" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot5" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+					</tr>
+					<tr>
+						<td id="slot6" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot7" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot8" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot9" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot10" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+					</tr>
+					<tr>
+						<td id="slot11" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot12" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot13" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot14" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot15" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+					</tr>
+					<tr>
+						<td id="slot16" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot17" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot18" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot19" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot20" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+					</tr>
+					<tr>
+						<td id="slot21" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot22" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot23" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot24" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+						<td id="slot25" data-tooltipimg="" data-tooltiptext=""><img class="tooltipQ" src="question_mark.png"></td>
+					</tr>
+				</table>
+				<div id="hidden-bingo">
+					<div id="hidden-table">
+						<div id="hidden-table-seed">
+							Bingo Seed:<br>
+							<input class="seed_for_copying" id="seed_for_copying_hidden" type="text" value="12345" readonly="readonly"><br>
+							<button onclick='copySeedToClipboard("seed_for_copying_hidden");'>Copy Seed to Clipboard</button>
+						</div>
+						<p>Use this seed to create a new world in Minecraft. Remember to adjust ingame settings such as difficulty if necessary.</p>
+						<p>If you've been linked this page for a race, you are not allowed to change the seed/goals. See the full rules below.</p>
+						<button class="button show-table-button" onclick='toggleHidden();event.preventDefault();'>Show Table</button>
+						<p>The table is currently hidden, you may show it when the Bingo starts (unless otherwise agreed upon by the players).</p>
+					</div>
 				</div>
-				<p>Use this seed to create a new world in Minecraft. Remember to adjust ingame settings such as difficulty if necessary.</p>
-				<p>If you've been linked this page for a race, you are not allowed to change the seed/goals. See the full rules below.</p>
-				<button class="button show-table-button" onclick='toggleHidden();event.preventDefault();'>Show Table</button>
-				<p>The table is currently hidden, you may show it when the Bingo starts (unless otherwise agreed upon by the players).</p>
 			</div>
 			<div id="info_bar">
 				Seed: <input class="seed_for_copying" type="text" value="12345" readonly="readonly" />&emsp;Difficulty: <span class="stream-difficulty-text"></span>&emsp;Version: <span class="versionText"></span><br>

commit 48ce64b8dc1a072b32ccd98ab97dfe5c32485fa6

hide .tooltipQ using CSS attribute rule
Hiding and showing of the tooltip icon .tooltipQ was broken by the
recent commit fa40258 (js: hide the whole #bingo on "Hide Table" click,
2021-03-06), because it removed a bit of code which touched visibility
of tooltips.

Steps to reproduce:

  1. Open a URL with a hidden table setting, for example
     index.html?s=4-1-0-dev_89001
  2. Click "Show Table"
  3. Observe the squares which are supposed to have tooltips,
     e.g. "Activate a 5x4 Nether Portal (not counting corners)".

So overhaul the .tooltipQ showing/hiding code simplify it.  This also
fixes the bug.  Set the data attributes to individual squares using the
attr() setter.  We have to use `attr()` setter, rather than `data()`,
because, quote from jQuery documentation [1]:

> Using the `data()` method to update data does not affect attributes
> in the DOM. To set a `data-*` attribute value, use `attr`.

So use attr() to set data attributes of squares in generateNewSheet(),
thus making it available to new CSS selectors.  Hide the .tooltipQ using
CSS, when the data attribute for the tooltip text in the <td> tags
(bingo squares) is empty.

[1] https://api.jquery.com/data/ see also
    https://stackoverflow.com/a/13524139/1083697

bingo.css
@@ -237,10 +237,6 @@ html, body {
 	background:#a77039;
 }
 
-#bingo td img {
-	visibility: hidden;
-}
-
 #hidden-table {
 	font-family: settingsFont;
 	background-color: white;
@@ -283,6 +279,9 @@ html, body {
 	padding-top: 4px;
 	right: 5px;
 	top: 5px;
+}
+#bingo td[data-tooltiptext=""] .tooltipQ {
+	display: none;
 }
 
 /* End Bingo Card  */

bingo.js
@@ -37,6 +37,8 @@ var LATEST_VERSION = "3";
 
 const SQUARE_COUNT = 25;
 const NODE_TYPE_TEXT = 3;
+const TOOLTIP_TEXT_ATTR_NAME = "data-tooltiptext";
+const TOOLTIP_IMAGE_ATTR_NAME = "data-tooltipimg";
 
 // Dropdown menu handling.
 $(document).click(function(event) {
@@ -318,9 +320,6 @@ function generateNewSheet()
 	// Reset every goal square
 	forEachSquare((i, square) => {
 		square.contents().filter(function(){ return this.nodeType == NODE_TYPE_TEXT; }).remove();
-		square.data("tooltipimg", "");
-		square.data("tooltiptext", "");
-		square.children().css("visibility", "hidden");
 		setSquareColor(square, "");
 	});
 
@@ -332,22 +331,8 @@ function generateNewSheet()
 		//square.append(goal.generatedName + " " + goal.difficulty);
 		square.append(goal.generatedName);
 		
-		if (typeof goal.tooltipimg !== 'undefined')
-		{
-			square.data("tooltipimg", goal.tooltipimg);
-			if (!HIDDEN)
-			{
-				square.children().css("visibility", "visible");
-			}
-		}
-		if (typeof goal.tooltiptext !== 'undefined')
-		{
-			square.data("tooltiptext", goal.tooltiptext);
-			if (!HIDDEN)
-			{
-				square.children().css("visibility", "visible");
-			}
-		}
+		square.attr(TOOLTIP_TEXT_ATTR_NAME, goal.tooltiptext || "");
+		square.attr(TOOLTIP_IMAGE_ATTR_NAME, goal.tooltipimg || "");
 	});
 }
 