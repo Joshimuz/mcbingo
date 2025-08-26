79080fa755efc4035b7a4860d0b8d203bbef0549
EsBaTix <esbatix+github@posteo.net>
2025-08-26 20:27:02 +0200

    adding rudimentary saving and loading functionality

--- bingo.js
+++ bingo.js
@@ -773,3 +773,50 @@ function gup( name )
 
 // random source: www.engin33r.net/bingo/random.js
 (function(j,i,g,m,k,n,o){function q(b){var e,f,a=this,c=b.length,d=0,h=a.i=a.j=a.m=0;a.S=[];a.c=[];for(c||(b=[c++]);d<g;)a.S[d]=d++;for(d=0;d<g;d++)e=a.S[d],h=h+e+b[d%c]&g-1,f=a.S[h],a.S[d]=f,a.S[h]=e;a.g=function(b){var c=a.S,d=a.i+1&g-1,e=c[d],f=a.j+e&g-1,h=c[f];c[d]=h;c[f]=e;for(var i=c[e+h&g-1];--b;)d=d+1&g-1,e=c[d],f=f+e&g-1,h=c[f],c[d]=h,c[f]=e,i=i*g+c[e+h&g-1];a.i=d;a.j=f;return i};a.g(g)}function p(b,e,f,a,c){f=[];c=typeof b;if(e&&c=="object")for(a in b)if(a.indexOf("S")<5)try{f.push(p(b[a],e-1))}catch(d){}return f.length?f:b+(c!="string"?"\0":"")}function l(b,e,f,a){b+="";for(a=f=0;a<b.length;a++){var c=e,d=a&g-1,h=(f^=e[a&g-1]*19)+b.charCodeAt(a);c[d]=h&g-1}b="";for(a in e)b+=String.fromCharCode(e[a]);return b}i.seedrandom=function(b,e){var f=[],a;b=l(p(e?[b,j]:arguments.length?b:[(new Date).getTime(),j,window],3),f);a=new q(f);l(a.S,j);i.random=function(){for(var c=a.g(m),d=o,b=0;c<k;)c=(c+b)*g,d*=g,b=a.g(1);for(;c>=n;)c/=2,d/=2,b>>>=1;return(c+b)/d};return b};o=i.pow(g,m);k=i.pow(2,k);n=k*2;l(i.random(),j)})([],Math,256,6,52);
+
+function progressToBase64(progress) {
+	return btoa(unescape(encodeURIComponent(JSON.stringify(progress))));
+}
+
+function progressFromBase64(b64) {
+	try {
+		return JSON.parse(decodeURIComponent(escape(atob(b64))));
+	}
+	catch (e) {
+		console.error("Invalid Base64 or JSON:", e);
+		return;
+	}
+}
+
+function saveProgress() {
+	var difficulty = DIFFICULTY;
+	var version = VERSION.id;
+	seed = SEED;
+	var slots = [];
+	var slotElements = document.querySelectorAll("#bingo td");
+
+	slotElements.forEach(cell => {
+		slots.push({
+			id: cell.id,
+			classes: cell.className.split(" ").filter(Boolean)
+		});
+	});
+
+	const progress = { difficulty, version, seed, slots };
+	console.log(progress);
+	console.log(progressToBase64(progress));
+}
+
+function loadProgress(base64String) {
+	const progress = progressFromBase64(base64String);
+
+	console.log(progress);
+	changeSeed(progress.seed);
+	changeDifficulty(progress.difficulty);
+	changeVersion(progress.version);
+
+	progress.slots.forEach(item => {
+		const element = document.getElementById(item.id);
+		element.className = item.classes.join(" ");
+	});
+}
